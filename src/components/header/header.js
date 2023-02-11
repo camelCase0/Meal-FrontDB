59a4c6ea5fcc75a1037a557e19d385e43eeacfc6
EnotikPoloskyn55 <staszinovatnyj@gmail.com>
2023-02-11 12:41:39 +0200

    nothing

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,6 +12,7 @@ export default class Header extends Component {
         <div className="col-2 col-md-2 col-lg-1">
           <button className="hrefmain">
             <Link to="/">Main</Link>
+            <img></img>
           </button>
         </div>
         <div className="col-2 col-md-2 col-lg-1">
